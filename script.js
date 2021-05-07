822b5b9cec0fb28504686d96eaa6fbb540ff64bb
pavlogrechka <pavlogrechka@gmail.com>
2021-05-07 22:28:11 +0300

    fix burger add overlay

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-'use strict';
+"use strict";
 
-//get css variables
-const root = document.querySelector(':root');
-const rootStyles = getComputedStyle(root);
-const mainColor = rootStyles.getPropertyValue('--main-color');
+// //get css variables
+// const root = document.querySelector(':root');
+// const rootStyles = getComputedStyle(root);
+// const mainColor = rootStyles.getPropertyValue('--main-color');
 
-//change css variables
-root.style.setProperty('--main-color', 'green');
-console.log(mainColor)
-
+// //change css variables
+// root.style.setProperty('--main-color', 'green');
+// console.log(mainColor)
 
 /**
  * forEach implementation for Objects/NodeLists/Arrays, automatic type loops and context options
@@ -22,23 +21,28 @@ console.log(mainColor)
  * @returns {}
  */
 const forEach = function (t, o, r) {
-  if ('[object Object]' === Object.prototype.toString.call(t))
+  if ("[object Object]" === Object.prototype.toString.call(t))
     for (var c in t) {
       Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
-    } else {
-    for (var e = 0, l = t.length; l > e; e++)o.call(r, t[e], e, t)
+    }
+  else {
+    for (var e = 0, l = t.length; l > e; e++) o.call(r, t[e], e, t);
   }
 };
 
 const hamburgers = document.querySelectorAll(".hamburger");
+// const overlay = document.querySelector('.nav-overlay')
 if (hamburgers.length > 0) {
   forEach(hamburgers, function (hamburger) {
-    hamburger.addEventListener("click", function () {
-      this.classList.toggle("is-active");
-    }, false);
+    hamburger.addEventListener(
+      "click",
+      function () {
+        this.classList.toggle("is-active");
+      },
+      false
+    );
   });
 }
-
 
 //GET DEVICE TYPE
 
@@ -64,16 +68,16 @@ const isMobile = {
       isMobile.BlackBerry() ||
       isMobile.iOs() ||
       isMobile.Opera() ||
-      isMobile.Windows());
-  }
+      isMobile.Windows()
+    );
+  },
 };
 // add a class for the body depending on isMobile() result
 if (isMobile.any()) {
-  document.body.classList.add('_touch');
+  document.body.classList.add("_touch");
 } else {
-  document.body.classList.add('_pc');
+  document.body.classList.add("_pc");
 }
-
 
 //scroll to fixed sections
 // const menuLinks = document.querySelectorAll('menu__link[data-goto]');
