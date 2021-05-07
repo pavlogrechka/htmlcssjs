45f225c9530811182fb1f89bc7188e7ee79de4a4
pavlogrechka <pavlogrechka@gmail.com>
2021-05-07 16:17:37 +0300

    add code

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 'use strict';
 
-function duplicateCount(text) {
+//get css variables
+const root = document.querySelector(':root');
+const rootStyles = getComputedStyle(root);
+const mainColor = rootStyles.getPropertyValue('--main-color');
 
-  debugger;
+//change css variables
+root.style.setProperty('--main-color', 'green');
+console.log(mainColor)
 
-  let result = 0;
 
-  const arr = text.split('');
-
-  let counter = 0;
-  for (let i = 0; i < text.length; i++) {
-    const el = text[i];
-    arr.includes(el) ? counter++ : counter;
-    counter >= 2 ? result++ : result
-    console.log(counter)
+/**
+ * forEach implementation for Objects/NodeLists/Arrays, automatic type loops and context options
+ *
+ * @private
+ * @author Todd Motto
+ * @link https://github.com/toddmotto/foreach
+ * @param {Array|Object|NodeList} collection - Collection of items to iterate, could be an Array, Object or NodeList
+ * @callback requestCallback      callback   - Callback function for each iteration.
+ * @param {Array|Object|NodeList} scope=null - Object/NodeList/Array that forEach is iterating over, to use as the this value when executing callback.
+ * @returns {}
+ */
+const forEach = function (t, o, r) {
+  if ('[object Object]' === Object.prototype.toString.call(t))
+    for (var c in t) {
+      Object.prototype.hasOwnProperty.call(t, c) && o.call(r, t[c], c, t);
+    } else {
+    for (var e = 0, l = t.length; l > e; e++)o.call(r, t[e], e, t)
   }
+};
 
-
-  console.log(result)
-
+const hamburgers = document.querySelectorAll(".hamburger");
+if (hamburgers.length > 0) {
+  forEach(hamburgers, function (hamburger) {
+    hamburger.addEventListener("click", function () {
+      this.classList.toggle("is-active");
+    }, false);
+  });
 }
-console.log(duplicateCount("aabbbcd"));
